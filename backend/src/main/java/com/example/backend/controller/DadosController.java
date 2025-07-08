9474a2e76194fc9f8f9088cd25ed05d848005a49
Davi <obrigatoriopcional@gmail.com>
2025-07-08 20:28:34 -0300

    add basic backend

---
+++ backend/src/main/java/com/example/backend/controller/DadosController.java
@@ -0,0 +1,19 @@
+package com.exemplo.backend.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/dados")
+@CrossOrigin(origins = "http:localhost:4200")
+public class DadosController{
+    @GetMapping
+    public List<Map<String, String>> listarDados(){
+        return List.of(
+                Map.of("id", "1", "nome", "Item 1"),
+                Map.of("id", "2", "nome", "Item 2"),
+        );
+    }
+}
