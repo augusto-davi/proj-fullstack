7ba6b15d3f42e95d20224685c80a5701e97f1e71
Davi <obrigatoriopcional@gmail.com>
2025-07-09 14:39:39 -0300

    fix springboot config

--- backend/src/main/java/com/example/backend/controller/DadosController.java
+++ backend/src/main/java/com/example/backend/controller/DadosController.java
@@ -1,4 +1,4 @@
-package com.exemplo.backend.controller;
+package com.example.backend.controller;
 
 import org.springframework.web.bind.annotation.*;
 
@@ -7,13 +7,13 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/dados")
-@CrossOrigin(origins = "http:localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class DadosController{
     @GetMapping
     public List<Map<String, String>> listarDados(){
         return List.of(
                 Map.of("id", "1", "nome", "Item 1"),
-                Map.of("id", "2", "nome", "Item 2"),
+                Map.of("id", "2", "nome", "Item 2")
         );
     }
 }
