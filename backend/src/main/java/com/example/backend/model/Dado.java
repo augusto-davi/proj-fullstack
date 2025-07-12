62ec53f0470e4b8a2dc2cd4abbc23acaf1748d32
Davi <obrigatoriopcional@gmail.com>
2025-07-11 21:22:35 -0300

    update model Dado

--- backend/src/main/java/com/example/backend/model/Dado.java
+++ backend/src/main/java/com/example/backend/model/Dado.java
@@ -1,5 +1,29 @@
 package com.example.backend.model;
 
 public class Dado {
+    private String id;
+    private String nome;
 
+    public Dado(){}
+
+    public Dado(String id, String nome){
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
