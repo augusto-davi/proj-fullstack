ad986e2957ba17402c7cbc460bb48a0a74507f55
Davi <obrigatoriopcional@gmail.com>
2025-07-11 21:36:36 -0300

    update DadosController

--- backend/src/main/java/com/example/backend/controller/DadosController.java
+++ backend/src/main/java/com/example/backend/controller/DadosController.java
@@ -1,5 +1,7 @@
 package com.example.backend.controller;
 
+import com.example.backend.model.Dado;
+import com.example.backend.repository.DadoRepositorio;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -12,21 +14,28 @@ import java.util.Map;
 @CrossOrigin(origins = "http://localhost:4200")
 public class DadosController {
 
-    @GetMapping
-    public List<Map<String, String>> listarDados() {
-        List<Map<String, String>> dados = new ArrayList<>();
+    private final DadoRepositorio repositorio;
 
-        Map<String, String> item1 = new HashMap<>();
-        item1.put("id", "1");
-        item1.put("nome", "Item 1");
-
-        Map<String, String> item2 = new HashMap<>();
-        item2.put("id", "2");
-        item2.put("nome", "Item 2");
-
-        dados.add(item1);
-        dados.add(item2);
-
-        return dados;
+    public DadosController(DadoRepositorio repositorio){
+        this.repositorio = repositorio;
     }
+
+    @GetMapping
+    public List<Dado> listar(){
+        return repositorio.listarTodos();
+    }
+
+    @PostMapping
+    public void adicionar(@RequestBody Dado dado){
+        repositorio.adicionar(dado);
+    }
+
+    @DeleteMapping("/{id}")
+    public void remover(@PathVariable String id){
+        repositorio.removerPorId(id);
+    }
+
+
+
+
 }
