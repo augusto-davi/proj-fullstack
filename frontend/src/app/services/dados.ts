a227cada9f64170d4a22e8144050e240097f978e
Davi <obrigatoriopcional@gmail.com>
2025-07-24 16:57:25 -0300

    update dados service

--- frontend/src/app/services/dados.ts
+++ frontend/src/app/services/dados.ts
@@ -2,13 +2,30 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+export interface Dado{
+  id: string;
+  nome: string;
+}
+
 @Injectable({ providedIn: 'root' })
 export class Dados {
-  private apiUrl = 'http://localhost:8080/api/dados';
+  private readonly apiUrl = 'http://localhost:8080/api/dados';
 
   constructor(private http: HttpClient){}
 
-  listarDados(): Observable<any[]>{
-    return this.http.get<any[]>(this.apiUrl);
+  listar(): Observable<Dado[]>{
+    return this.http.get<Dado[]>(this.apiUrl);
+  }
+
+  adicionar(dado: Dado): Observable<void>{
+    return this.http.post<void>(this.apiUrl, dado);
+  }
+
+  atualizar(dado: Dado): Observable<void>{
+    return this.http.put<void>(this.apiUrl, dado);
+  }
+
+  remover(id: string): Observable<void>{
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 }
